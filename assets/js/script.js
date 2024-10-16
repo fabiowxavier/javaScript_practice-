af2c603755a9d0f78ca7bfc5339a3fef250e3e13
Fabio Williams-xavier <fabiobmk@gmail.com>
2024-10-16 22:24:27 +0000

    add reset button to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
 let count = 0;
 let btn = document.getElementById("button");
+let resetBtn = document.getElementById("reset-button");
 let numDisplayed = document.getElementById("display");
 
 btn.addEventListener("click", function () {
     count++;
+    numDisplayed.innerHTML = count;
+});
+resetBtn.addEventListener("click", function () {
+    count = 0;
     numDisplayed.innerHTML = count;
 });
